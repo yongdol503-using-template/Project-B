2124d452e53c85ce96fe740e95f403e668715ec3
yongdol503 <yongdol503@gmail.com>
2024-12-11 17:13:08 +0900

    add unit_test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+RED="\033[1;31m"
+RESET="\033[0m"
+
+echo -e "${GREEN}build.sh: Building the package${RESET}"
+
+set +e
 colcon build --packages-select calculator_pkg --symlink-install
-source install/setup.bash
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}build.sh: Finished building the package${RESET}"
+else
+    echo -e "${RED}build.sh: Failed to build the package${RESET}"
+fi
--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Run unit tests
-echo "Running unit tests"
+echo -e "\033[1;32munit_test.sh: Running unit tests\033[0m"
+
+set +e
 colcon test --packages-select calculator_pkg
-colcon test-result --verbose
+colcon test-result --all
+
+if [ $? -eq 0 ]; then
+    echo -e "\033[1;34munit_test.sh: All tests passed\033[0m"
+else
+    echo -e "\033[1;31munit_test.sh: Some tests failed\033[0m"
+fi
+echo -e "\033[1;32munit_test.sh: Finished running unit tests\033[0m"
 
 # Todo: add coverage report
